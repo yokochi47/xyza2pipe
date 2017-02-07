e10370ac5c1b0e681b74317e457b2a06e66e54ce
Masashi Yokochi <yokochi47@gmail.com>
2017-02-07 16:35:56 +0900

    Add files via upload

--- libString.h
+++ libString.h
@@ -1,4 +1,10 @@
 /*
+   xyza2pipe - a cross conversion environment of NMR spectra
+   Copyright 2017 Masashi Yokochi
+
+   https://github.com/yokochi47/xyza2pipe
+    forked from http://fermi.pharm.hokudai.ac.jp/olivia/
+
 Licensed under the Apache License, Version 2.0 (the "License");
 you may not use this file except in compliance with the License.
 You may obtain a copy of the License at
@@ -10,10 +16,6 @@ distributed under the License is distributed on an "AS IS" BASIS,
 WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 See the License for the specific language governing permissions and
 limitations under the License.
- */
-
-/* xyza2pipe: Cross conversion environment of NMR spectra
-   https://github.com/yokochi47/xyza2pipe forked from http://fermi.pharm.hokudai.ac.jp/olivia/
  */
 
 #ifndef __LIB_STRING__
